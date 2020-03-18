d0688ffea868f3bfc64744a420510b5a8ea1bb57
André Martins <andre@basedigital.pt>
2020-03-18 00:00:50 +0000

    Added Palette

---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Palette = () => {
+  return (
+    <div className="Palette">
+      <di className="Palete-colors">Palette</di>
+    </div>
+  );
+};
+
+export default Palette;
